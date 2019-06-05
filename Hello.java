be4640ed64512be1ff964e09c807e664920683e3
Silvio123silva <44685991+Silvio123silva@users.noreply.github.com>
2019-06-05 19:50:32 -0300

    hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+* Hello World Java
+* @author Silvio Andrade
+*/
+
+public class Hello{
+	//Método principal(inicio de tudo)
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
